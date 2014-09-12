ceea727a20ccd3e3d2db888de100975c71fe6d82
Justin Peacock <justin@sixteenbit.com>
2014-09-12 09:52:07 -0400

    prepping for release

--- device-mockups.php
+++ device-mockups.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Device Mockups
 Plugin URI: https://wordpress.org/plugins/device-mockups/
-Description: Simplify your mockups. Device Mockups generates shortcodes that display screenshots of your content in a responsive device.
+Description: Show your work in high resolution, responsive device mockups using only shortcodes.
 Author: Justin Peacock
 Version: 1.3.0
 Author URI: http://byjust.in
@@ -11,8 +11,8 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 function DM_add_stylesheet() {
-  wp_register_style( 'DM-style', plugins_url('assets/css/dm-style.min.css', __FILE__), false, '1.3.0' );
-  wp_enqueue_style( 'DM-style' );
+	wp_register_style( 'DM-style', plugins_url('assets/css/dm-style.min.css', __FILE__), false, '1.3.0' );
+	wp_enqueue_style( 'DM-style' );
 }
 add_action( 'wp_enqueue_scripts', 'DM_add_stylesheet' );
 
@@ -22,59 +22,59 @@ add_action( 'wp_enqueue_scripts', 'DM_add_stylesheet' );
 function device_wrapper( $atts , $content = null ) {
   // Attributes
   extract( shortcode_atts(
-    array(
-        'type' => '',
-        'orientation' => '',
-        'color' => '',
-        'stacked' => '',
-        'position' => '',
-        'link' => '',
-        'width' => '',
-        'hide' => ''
-    ), $atts )
+	array(
+		'type' => '',
+		'orientation' => '',
+		'color' => '',
+		'stacked' => '',
+		'position' => '',
+		'link' => '',
+		'width' => '',
+		'hide' => ''
+	), $atts )
   );
   ob_start();
 
   // Open $stacked
   if (esc_attr($stacked) == 'open') { echo '<div class="dm-stacked">';  }
 
-    // Open $hide
-    if ( !empty( $hide ) ) { echo '<div class="dm-hide-'. esc_attr( $hide ) .'">'; }
+	// Open $hide
+	if ( !empty( $hide ) ) { echo '<div class="dm-hide-'. esc_attr( $hide ) .'">'; }
 
-      // Open $width
-      if ( !empty( $width ) ) { echo '<div class="dm-width" style="width:'. $width .';">';  } ?>
+	  // Open $width
+	  if ( !empty( $width ) ) { echo '<div class="dm-width" style="width:'. $width .';">';  } ?>
 
-        <div class="<?php
-          if ( !empty( $position ) ) {
-            echo 'dm-stacked-'. esc_attr( $position ) .''; }
-          if ( !empty( $type ) ) {
-            echo ' '. esc_attr( $type ) .''; }
-          if ( !empty( $orientation ) ) {
-            echo ' '. esc_attr( $orientation ) .''; } ?>">
+		<div class="<?php
+		  if ( !empty( $position ) ) {
+			echo 'dm-stacked-'. esc_attr( $position ) .''; }
+		  if ( !empty( $type ) ) {
+			echo ' '. esc_attr( $type ) .''; }
+		  if ( !empty( $orientation ) ) {
+			echo ' '. esc_attr( $orientation ) .''; } ?>">
 
-          <div class="dm-device" <?php
-            if ( !empty( $type ) ) {
-              echo 'data-device="'. esc_attr( $type ) .'"'; }
-            if ( !empty( $orientation ) ) {
-              echo ' data-orientation="'. esc_attr( $orientation ) .'"'; }
-            if ( !empty( $color ) ) {
-              echo ' data-color="'. esc_attr( $color ) .'"'; } ?>>
-        <?php
-            echo '<div class="device">';
-              echo '<div class="screen">';
-                if ( !empty($link) ) { echo '<a href="' . esc_attr($link) . '">'; }
-                  echo '' . do_shortcode($content) . '';
-                if ( !empty($link) ) { echo '</a>'; }
-              echo '</div>';
-            echo '</div>';
-          echo '</div>';
-        echo '</div>';
+		  <div class="dm-device" <?php
+			if ( !empty( $type ) ) {
+			  echo 'data-device="'. esc_attr( $type ) .'"'; }
+			if ( !empty( $orientation ) ) {
+			  echo ' data-orientation="'. esc_attr( $orientation ) .'"'; }
+			if ( !empty( $color ) ) {
+			  echo ' data-color="'. esc_attr( $color ) .'"'; } ?>>
+		<?php
+			echo '<div class="device">';
+			  echo '<div class="screen">';
+				if ( !empty($link) ) { echo '<a href="' . esc_attr($link) . '">'; }
+				  echo '' . do_shortcode($content) . '';
+				if ( !empty($link) ) { echo '</a>'; }
+			  echo '</div>';
+			echo '</div>';
+		  echo '</div>';
+		echo '</div>';
 
-      // Close $width
-      if ( !empty( $width ) ) { echo '</div>'; }
+	  // Close $width
+	  if ( !empty( $width ) ) { echo '</div>'; }
 
-    // Close $hide
-    if ( !empty( $hide ) ) { echo '</div>'; }
+	// Close $hide
+	if ( !empty( $hide ) ) { echo '</div>'; }
 
   // Close $stacked
   if (esc_attr($stacked) == 'closed') { echo '</div>';  }
@@ -90,37 +90,37 @@ function browser_wrapper( $atts , $content = null ) {
 
   // Attributes
   extract( shortcode_atts(
-    array(
-      'type' => 'chrome',
-      'link' => '',
-      'width' => '',
-    ), $atts )
+	array(
+	  'type' => 'chrome',
+	  'link' => '',
+	  'width' => '',
+	), $atts )
   );
 
   ob_start();
 
-    // Open $width
-    if ( !empty( $width ) ) { echo '<div style="max-width:'. $width .'">';  } ?>
+	// Open $width
+	if ( !empty( $width ) ) { echo '<div style="max-width:'. $width .'">';  } ?>
 
-        <div class="dm-browser" <?php
-            if ( !empty( $type ) ) {
-              echo 'data-device="'. esc_attr( $type ) .'"'; }
-            if ( !empty( $orientation ) ) {
-              echo ' data-orientation="'. esc_attr( $orientation ) .'"'; }
-            if ( !empty( $color ) ) {
-              echo ' data-color="'. esc_attr( $color ) .'"'; } ?>>
-        <?php
-            echo '<div class="device">';
-                echo '<div class="screen">';
-                    if ( !empty($link) ) { echo '<a href="' . esc_attr($link) . '">'; }
-                      echo '' . do_shortcode($content) . '';
-                    if ( !empty($link) ) { echo '</a>'; }
-                echo '</div>';
-            echo '</div>';
-        echo '</div>';
+		<div class="dm-browser" <?php
+			if ( !empty( $type ) ) {
+			  echo 'data-device="'. esc_attr( $type ) .'"'; }
+			if ( !empty( $orientation ) ) {
+			  echo ' data-orientation="'. esc_attr( $orientation ) .'"'; }
+			if ( !empty( $color ) ) {
+			  echo ' data-color="'. esc_attr( $color ) .'"'; } ?>>
+		<?php
+			echo '<div class="device">';
+				echo '<div class="screen">';
+					if ( !empty($link) ) { echo '<a href="' . esc_attr($link) . '">'; }
+					  echo '' . do_shortcode($content) . '';
+					if ( !empty($link) ) { echo '</a>'; }
+				echo '</div>';
+			echo '</div>';
+		echo '</div>';
 
-    // Close $width
-    if ( !empty( $width ) ) { echo '</div>'; }
+	// Close $width
+	if ( !empty( $width ) ) { echo '</div>'; }
 
   return ob_get_clean();
 }
@@ -132,24 +132,24 @@ add_shortcode( 'browser', 'browser_wrapper' );
 add_action('admin_head', 'DM_tinymce_button');
 
 function DM_tinymce_button() {
-    global $typenow;
-    // check user permissions
-    if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
-        return;
-    }
-    // verify the post type
-    if( ! in_array( $typenow, array( 'post', 'page' ) ) )
-        return;
+	global $typenow;
+	// check user permissions
+	if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
+		return;
+	}
+	// verify the post type
+	if( ! in_array( $typenow, array( 'post', 'page' ) ) )
+		return;
   // check if WYSIWYG is enabled
   if ( get_user_option('rich_editing') == 'true') {
-    add_filter("mce_external_plugins", "DM_add_tinymce_plugin");
-    add_filter('mce_buttons', 'DM_register_my_tc_button');
+	add_filter("mce_external_plugins", "DM_add_tinymce_plugin");
+	add_filter('mce_buttons', 'DM_register_my_tc_button');
   }
 }
 
 function DM_add_tinymce_plugin($plugin_array) {
-    $plugin_array['DM_tc_button'] = plugins_url( '/assets/js/editor.min.js', __FILE__ );
-    return $plugin_array;
+	$plugin_array['DM_tc_button'] = plugins_url( '/assets/js/editor.min.js', __FILE__ );
+	return $plugin_array;
 }
 
 function DM_register_my_tc_button($buttons) {
@@ -162,18 +162,27 @@ function DM_register_my_tc_button($buttons) {
 //
 function DM_tinymce_icon() { ?>
   <style type="text/css" media="screen">
-    i.mce-i-icon {
-        font: 400 20px/1 dashicons;
-        padding: 0;
-        vertical-align: top;
-        speak: none;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        margin-left: -2px;
-        padding-right: 2px
-    }
+	i.mce-i-icon {
+		font: 400 20px/1 dashicons;
+		padding: 0;
+		vertical-align: top;
+		speak: none;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		margin-left: -2px;
+		padding-right: 2px
+	}
   </style>
 <?php
 
 }
 add_action( 'admin_head', 'DM_tinymce_icon' );
+
+function DM_docs_link($links) {
+    $settings_link = '<a href="http://dm.byjust.in">Documentation</a>';
+    array_unshift($links, $settings_link);
+    return $links;
+}
+
+$plugin = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin", 'DM_docs_link' );
