7e8efe29a2abc2a5fa12beedcd8f40f649b7e38e
Justin Peacock <justin@sixteenbit.com>
2015-02-20 11:24:34 -0500

    started on BrowserHelper but didn’t finish need to re-write the browser_wrapper.php

---
+++ trunk/helpers/BrowserHelper.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace com\mrdink\device_mockups\helpers;
+
+class BrowserHelper {
+	public $attr;
+
+}
--- trunk/views/browser_wrapper.php
+++ trunk/views/browser_wrapper.php
@@ -1,18 +1,26 @@
-<?php function getDataAttribute() { $output='' ; if (!empty($type)) { $output .='data-device="' . esc_attr($type) . '"'; } if (!empty($orientation)) { $output .=' data-orientation="' . esc_attr($orientation) . '"'; } if (!empty($color)) { $output .=' data-color="' . esc_attr($color) . '"'; } return $output; } ?>
+<?php
+
+function getDataAttribute() {
+	$output='' ; if (!empty($type)) {
+		$output .='data-device="' . esc_attr($type) . '"';
+	}
+	if (!empty($orientation)) {
+		$output .=' data-orientation="' . esc_attr($orientation) . '"';
+	}
+	if (!empty($color)) {
+		$output .=' data-color="' . esc_attr($color) . '"';
+	} return $output; }
+?>
 
 <div class="wrap">
   <?php if (!empty($width)): ?>
-  <div style="max-width:<?= $width ?>">
+  <div style="max-width:<?=$width ?>">
     <?php endif ?>
 
-    <div class="dm-browser" <?=g etDataAttribute() ?>>
+    <div class="dm-browser" <?=getDataAttribute() ?>>
       <div class="device">
         <div class="screen">
-          <?php if (!empty($link)): ?>
-          <a href="<?= esc_attr($link) ?>">
-            <?=d o_shortcode($content) ?>
-          </a>
-          <?php endif ?>
+          <?php if (!empty($link)) { echo '<a href="' . esc_attr($link) . '">'; } echo do_shortcode($content); if (!empty($link)) { echo '</a>'; } ?>
         </div>
       </div>
     </div>
